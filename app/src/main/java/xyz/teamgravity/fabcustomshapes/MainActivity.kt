13e82601634c5f568787faee61a38403c1f10162
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-02 19:05:13 +0500

    setup view binding

--- app/src/main/java/xyz/teamgravity/fabcustomshapes/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/fabcustomshapes/MainActivity.kt
@@ -1,11 +1,16 @@
 package xyz.teamgravity.fabcustomshapes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import xyz.teamgravity.fabcustomshapes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
